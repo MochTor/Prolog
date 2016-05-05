64e6a422552b1fe45edfabbdd52310ae92e11225
MochTor <tieghi.marco@gmail.com>
2016-05-05 10:39:21 +0200

    Completed the program

--- Slipcover/trains.pl
+++ Slipcover/trains.pl
@@ -11,8 +11,52 @@
 :- set_sc(max_iter,3).
 :- set_sc(max_iter_structure,10).
 :- set_sc(maxdepth_var,4).
-:- set_sc(verbosity,1).
+:- set_sc(verbosity,3).
 
+
+in([]).
+
+fold(train,[east1, east2, east3, east4, east5, west6, west7, west8, west9, west10]).
+
+output(eastbound/0).    %ariety is 1 parameter less
+
+input_cw(short/1).
+input_cw(long/1).
+input_cw(closed/1).
+input_cw(open_car/1).
+input_cw(double/1).
+input_cw(jagged/1).
+input_cw(shape/2).
+input_cw(load/3).
+input_cw(wheels/2).
+input_cw(has_car/1).    %ariety is 1 parameter less
+
+modeh(1,eastbound).
+modeb(*,has_car(-car)).
+modeb(1,short(+car)).
+modeb(1,closed(+car)).
+modeb(1,long(+car)).
+modeb(1,open_car(+car)).
+modeb(1,double(+car)).
+modeb(1,jagged(+car)).
+modeb(1,shape(+car,#shape)).
+modeb(1,load(+car,#shape,#int)).
+modeb(1,wheels(+car,#int)).
+
+determination(eastbound/0,short/1).
+determination(eastbound/0,closed/1).
+determination(eastbound/0,long/1).
+determination(eastbound/0,open_car/1).
+determination(eastbound/0,double/1).
+determination(eastbound/0,jagged/1).
+determination(eastbound/0,shape/2).
+determination(eastbound/0,wheels/2).
+determination(eastbound/0,has_car/1).
+determination(eastbound/0,load/3).
+
+%%%%%%%%%%
+%database%
+%%%%%%%%%%
 :- begin_bg.
 car(car_11).  car(car_12).  car(car_13).  car(car_14).
 car(car_21).  car(car_22).  car(car_23).
@@ -204,50 +248,6 @@ load(car_102,rectangle,2).
 wheels(car_101,2).
 wheels(car_102,2).
 :- end_bg.
-
-in([]).
-
-fold(train,[east1, east2, east3, east4, east5, west6, west7, west8, west9, west10]).
-
-output(eastbound/0).
-
-input_cw(short/1).
-input_cw(long/1).
-input_cw(closed/1).
-input_cw(open_car/1).
-input_cw(double/1).
-input_cw(jagged/1).
-input_cw(shape/2).
-input_cw(load/3).
-input_cw(wheels/2).
-input_cw(has_car/2).
-
-modeh(1,eastbound(+train)).
-modeb(1,short(+car)).
-modeb(1,closed(+car)).
-modeb(1,long(+car)).
-modeb(1,open_car(+car)).
-modeb(1,double(+car)).
-modeb(1,jagged(+car)).
-modeb(1,shape(+car,#shape)).
-modeb(1,load(+car,#shape,#int)).
-modeb(1,wheels(+car,#int)).
-modeb(*,has_car(+train,-car)).
-
-determination(eastbound/1,short/1).
-determination(eastbound/1,closed/1).
-determination(eastbound/1,long/1).
-determination(eastbound/1,open_car/1).
-determination(eastbound/1,double/1).
-determination(eastbound/1,jagged/1).
-determination(eastbound/1,shape/2).
-determination(eastbound/1,wheels/2).
-determination(eastbound/1,has_car/2).
-determination(eastbound/1,load/3).
-
-%%%%%%%%%%
-%database%
-%%%%%%%%%%
 
 % eastbound train 1
 has_car(east1,car_11). % 11,12
